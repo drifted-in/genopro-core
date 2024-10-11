2aca40b7d62bca56ab22eb0e468da512aacb6e52
Jan Tošovský <jan.tosovsky.cz@gmail.com>
2024-10-11 17:27:31 +0200

    Adding module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module in.drifted.tools.genopro.core {
+
+    requires java.desktop;
+    requires java.xml;
+
+    exports in.drifted.tools.genopro.model;
+    exports in.drifted.tools.genopro.parser;
+    exports in.drifted.tools.genopro.util;
+
+}
